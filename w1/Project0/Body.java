e19ba60274f8ec5929b66bf1dbd5c6fdd95241c9
freddy <zhonglyu173@gmail.com>
2020-04-29 16:53:53 -0700

    Init commit for Project0: Finished Body Constructor.

---
+++ w1/Project0/Body.java
@@ -0,0 +1,34 @@
+package Project0;
+
+public class Body {
+  public double xxPos;
+  public double yyPos;
+  public double xxVel;
+  public double yyVel;
+  public double mass;
+  public String imgFileName;
+
+  public Body(double xP, double yP, double xV,
+              double yV, double m, String img) {
+    xxPos = xP;
+    yyPos = yP;
+    xxVel = xV;
+    yyVel = yV;
+    mass = m;
+    imgFileName = img;
+  }
+
+  /**
+   * The second constructor takes in a Body object
+   * and initialize an identical Body object.
+   * @param b a body object
+   */
+  public Body(Body b) {
+    xxPos = b.xxPos;
+    yyPos = b.yyPos;
+    xxVel = b.xxVel;
+    yyVel = b.yyVel;
+    mass = b.mass;
+    imgFileName = b.imgFileName;
+  }
+}
