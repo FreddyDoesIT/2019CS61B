076bee3873f30e5fda7e3b126f398b414d16e32b
Frederick <freddy@Freddys-Mac.local>
2020-08-20 18:13:29 -0700

    Finished ArrayRingBuffer and its unit test.

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,11 +1,7 @@
 package es.datastructur.synthesizer;
 import java.util.Iterator;
 
-//TODO: Make sure to that this class and all of its methods are public
-//TODO: Make sure to add the override tag for all overridden methods
-//TODO: Make sure to make this class implement BoundedQueue<T>
-
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T>  implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -19,17 +15,44 @@ public class ArrayRingBuffer<T>  {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+    }
+
+    /**
+     *
+     * @return return size of the buffer
+     */
+    @Override
+    public int capacity() {
+        return rb.length;
+    }
+
+    /**
+     *
+     * @return number of items currently in the buffer
+     */
+    @Override
+    public int fillCount() {
+        return this.fillCount;
     }
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update
-        //       last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last++] = x;
+        if (last == capacity()) {
+            last = updateIndex(last);
+        }
+        fillCount++;
         return;
     }
 
@@ -37,20 +60,36 @@ public class ArrayRingBuffer<T>  {
      * Dequeue oldest item in the ring buffer. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and
-        //       update first.
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T res = rb[first];
+//        rb[first] = null; // this actually does not matter.
+        first++;
+        if (first == capacity()) {
+            first = updateIndex(first);
+        }
+        fillCount--;
+        return res;
+    }
+
+    private int updateIndex (int index) {
+        return (index + 1) % this.capacity();
     }
 
     /**
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should
-        //       change.
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+
+        return rb[first];
     }
 
     // TODO: When you get to part 4, implement the needed code to support
--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,66 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(5);
+        // test capacity
+        int expected = 5;
+        int actual = arb.capacity();
+        assertEquals(expected, actual);
+        // test fillCount
+        expected = 0;
+        actual = arb.fillCount();
+        assertEquals(expected, actual);
+        // test enqueue
+        arb.enqueue(100);
+        expected = 1;
+        actual = arb.fillCount();
+        assertEquals(expected, actual);
+        // test peek
+        expected = 100;
+        actual = (int)arb.peek();
+        assertEquals(expected, actual);
+        // test dequeue
+        expected = 100;
+        actual = (int)arb.dequeue();
+        assertEquals(expected, actual);
+
+        expected = 0;
+        actual = arb.fillCount();
+        assertEquals(expected, actual);
+
+        ArrayRingBuffer arb2 = new ArrayRingBuffer(5);
+        // test first index
+        arb2.enqueue(101);
+        arb2.enqueue(102);
+        arb2.enqueue(103);
+        arb2.enqueue(104);
+        arb2.enqueue(105);
+
+        try{
+            arb2.enqueue(106);
+        } catch (RuntimeException ex) {
+            System.out.println("rb overflow.");
+        }
+
+        expected = 101;
+        actual = (int)arb2.dequeue();
+        assertEquals(expected, actual);
+
+        expected = 102;
+        actual = (int)arb2.peek();
+        assertEquals(expected, actual);
+
+        expected = 102;
+        actual = (int)arb2.dequeue();
+        assertEquals(expected, actual);
+
+        arb2.dequeue();
+        arb2.dequeue();
+        arb2.dequeue();
+
+        arb2.enqueue(1000);
+        expected = 1000;
+        actual = (int)arb2.peek();
+        assertEquals(expected, actual);
     }
 }
