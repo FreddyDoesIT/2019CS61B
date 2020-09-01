62d1ab2315b7714ccd05e7222339d545cda22d0e
Frederick <freddy@Freddys-Mac.local>
2020-09-01 16:37:36 -0700

    Finished clab7.

--- clab7/Experiments.java
+++ clab7/Experiments.java
@@ -40,12 +40,84 @@ public class Experiments {
     }
 
     public static void experiment2() {
+        bst = new BST();
+        // step 1
+        for (int i = 0; i < 1000; i++) {
+            bst.add(RandomGenerator.getRandomInt(500));
+        }
+        // starting depth
+        double depth = bst.averageDepth();
+
+        List<Double> depths = new ArrayList<>();
+        List<Double> startingDepth = new ArrayList<>();
+        for (int i = 0; i < 5000; i++) {
+            // step 2 random delete
+            bst.deleteTakingSuccessor(bst.getRandomKey());
+            // step 3 random add
+            int key = RandomGenerator.getRandomInt(1000);
+//            while (bst.contains(key)) {
+//                key = RandomGenerator.getRandomInt(1000);
+//            }
+            bst.add(key);
+            // step 4
+            depths.add(bst.averageDepth());
+            startingDepth.add(depth);
+        }
+
+        XYChart chart = new XYChartBuilder()
+                .width(800)
+                .height(600)
+                .xAxisTitle("Average Depth After Random Deletion and Insertion.")
+                .yAxisTitle("Starting Average Depth")
+                .build();
+
+        chart.addSeries("Starting Average Depth", startingDepth);
+        chart.addSeries("Average Depth", depths);
+
+        new SwingWrapper(chart).displayChart();
     }
 
     public static void experiment3() {
+        bst = new BST();
+        // step 1
+        for (int i = 0; i < 1000; i++) {
+            bst.add(RandomGenerator.getRandomInt(500));
+        }
+        // starting depth
+        double depth = bst.averageDepth();
+
+        List<Double> depths = new ArrayList<>();
+        List<Double> startingDepth = new ArrayList<>();
+        for (int i = 0; i < 5000; i++) {
+            // step 2 random delete
+            bst.deleteTakingRandom(bst.getRandomKey());
+            // step 3 random add
+            int key = RandomGenerator.getRandomInt(1000);
+//            while (bst.contains(key)) {
+//                key = RandomGenerator.getRandomInt(1000);
+//            }
+            bst.add(key);
+            // step 4
+            depths.add(bst.averageDepth());
+            startingDepth.add(depth);
+        }
+
+        XYChart chart = new XYChartBuilder()
+                .width(800)
+                .height(600)
+                .xAxisTitle("Average Depth After Random Deletion and Insertion.")
+                .yAxisTitle("Starting Average Depth")
+                .build();
+
+        chart.addSeries("Starting Average Depth", startingDepth);
+        chart.addSeries("Average Depth", depths);
+
+        new SwingWrapper(chart).displayChart();
     }
 
     public static void main(String[] args) {
-        experiment1();
+//        experiment1();
+//        experiment2();
+        experiment3();
     }
 }
